27d8f47c958ff850261337469064ab065cce4811
Hamed Seyed-allaei <hamed@ipm.ir>
2015-10-10 19:58:37 +0330

    Exercises

---
+++ fib.py
@@ -0,0 +1,18 @@
+def fib_loop(n):
+    '''
+    Returns the nth number in the fibonachi series.
+   
+    It uses for loop to calculate it.
+    '''
+
+    
+def fib_recursion(n):
+    '''
+    Returns the nth number in the fibonachi series.
+   
+    It uses for loop to calculate it.
+    '''
+
+
+print(fib_loop(8))
+print(fib_recursion(13))
