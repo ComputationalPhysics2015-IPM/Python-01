51f6a266a43c80826d4893f8483ea0e7c7e7dcfc
Hamed Seyed-allaei <hamed@ipm.ir>
2015-10-13 17:00:44 +0330

    Fibonacci generator.

--- fib.py
+++ fib.py
@@ -13,6 +13,11 @@ def fib_recursion(n):
     It uses for loop to calculate it.
     '''
 
+def fib_generator(n):
+    '''
+    Generator version of fibonacci.
+    '''
 
 print(fib_loop(8))
 print(fib_recursion(13))
+print([i for i in fib_generator(14)])
